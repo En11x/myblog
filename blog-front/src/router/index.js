264fa044860a47c3e1fe14918976de6b124d104c
pjx1211 <497421855@qq.com>
2019-12-08 23:15:12 +0800

    开发

--- blog-front/src/router/index.js
+++ blog-front/src/router/index.js
@@ -1,28 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HomePage from '@/pages/HomePage'
-import Home from '@/pages/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import HomePage from "@/pages/HomePage";
+import Home from "@/pages/Home.vue";
+import About from "@/pages/AboutMe";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/welcome',
-      name: 'HomePage',
-      component: HomePage,
+      path: "/welcome",
+      name: "HomePage",
+      component: HomePage
     },
     {
-      path: '/',
-      name: 'Home',
-      component: Home,
-      children: [
-        {
-          path: '/',
-          name: 'Home',
-          component: Home,
-        }
-      ]
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
     }
   ]
-})
+});
